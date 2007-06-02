2fe2f3d5491b141f47f314e4185c1d6754ca3c49
david.pettifer <david.pettifer@2420a164-f031-0410-b5c9-952b3439b043>
2007-06-02 20:19:36 +0000

    Deletedfailedimport
    git-svn-id: http://dizzy.googlecode.com/svn/trunk@3 2420a164-f031-0410-b5c9-952b3439b043

--- app/helpers/company_admin_helper.rb
+++
@@ -1,2 +0,0 @@
-module CompanyAdminHelper
-end
--- db/migrate/018_add_boolean_to_portfolio_types.rb
+++
@@ -1,9 +0,0 @@
-class AddBooleanToPortfolioTypes < ActiveRecord::Migration
-  def self.up
-  	add_column :portfolio_types, :visible, :boolean, :default => 1
-  end
-
-  def self.down
-  	remove_column :portfolio_types, :visible
-  end
-end
