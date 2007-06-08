19e2a122e0ed5e5728ec4cdc7f27bcbf48d16efb
david.pettifer <david.pettifer@2420a164-f031-0410-b5c9-952b3439b043>
2007-06-08 13:58:00 +0000

    Updating htaccess and dispatchers for A2Hosting
    git-svn-id: http://dizzy.googlecode.com/svn/trunk@15 2420a164-f031-0410-b5c9-952b3439b043

--- config/deploy.rb
+++ config/deploy.rb
@@ -1,34 +1,38 @@
 set :application, "dizzy"
 set :repository,  "http://dizzy.googlecode.com/svn/trunk"
 set :use_sudo, false
-set :user, "dizzynewmedia"   
-set :password, "Tipih9jq"
+set :user, "dizzynew"   
+set :password, "ruhegochelupraju"
 
 # If you aren't deploying to /u/apps/#{application} on the target
 # servers (which is the default), you can specify the actual location
 # via the :deploy_to variable:
 # set :deploy_to, "/var/www/#{application}"
 
-set :deploy_to, "/home/dizzynewmedia/rails_apps/#{application}"
+set :deploy_to, "/home/dizzynew/rails_apps/#{application}"
 set :deploy_via, :export
 
 # If you aren't using Subversion to manage your source code, specify
 # your SCM below:
 # set :scm, :subversion
 
-role :app, "thunderbird.dreamhost.com"
-role :web, "thunderbird.dreamhost.com"
-role :db,  "thunderbird.dreamhost.com", :primary => true
+role :app, "www21.a2hosting.com"
+role :web, "www21.a2hosting.com"
+role :db,  "www21.a2hosting.com", :primary => true
 
-set :svn_username, "david.pettifer"	
-set :svn_password, "j6y3h5d4" 
+set :svn_username, "dizzynew_david"	
+set :svn_password, "world1" 
+
+# SSH OPTIONS
+#=============================================================================
+ssh_options[:port] = 7822 
 
 	# DATABASE
 # =============================================================================
 set :database_adapter, "mysql"
-set :database_username, "dizzynewmedia"
-set :database_password, "Tipih9jq"
-set :database_host, "mysql.dizzynewmedia.dreamhosters.com"
+set :database_username, "dizzynew_melanie"
+set :database_password, "world1"
+set :database_hostname, "localhost"
 
 # =============================================================================
 desc "Tasks to execute after update"
@@ -36,11 +40,15 @@ task :after_update do
 	
 	# relink shared deployment database configuration
 	run "ln -nfs #{shared_path}/config/database.yml #{release_path}/config/database.yml"
+	
 	# relink rails vendor
 	# run "ln -nfs #{shared_rails_dir} #{release_path}/vendor/rails "
-	# chmod dispatch.fcgi and reaper
+	
+	# chmod dispatchers and reaper
 	run "chmod 755 #{release_path}/public/dispatch.fcgi"
+	run "chmod 755 #{release_path}/public/dispatch.cgi"
 	run "chmod 755 #{release_path}/script/process/reaper"
+	run "chmod 755 #{release_path}/public"
 end
 
 # =============================================================================
@@ -49,34 +57,38 @@ end
 
 desc "Construct database configuration"
 desc "Setup Database Configuration"
-namespace :deploy do
+
 	task :setup_database do
 
 	 # generate database configuration
-	 database_configuration = render :template => <<-EOF
-	# Deployment database.yml
-	login: &login
-	  adapter: #{database_adapter}
-	  host: #{database_host}
-	  username: #{database_username}
-	  password: #{database_password}
-	
-	development:
-	 database: #{application}_development
-	 <<: *login
-	
-	test:
-	 database: #{application}_test
-	 <<: *login
-	
-	production:
-	 database: #{application}_production
-	 <<: *login
-	
-	EOF
+	 database_configuration =  <<-EOF
+development:
+  adapter: #{database_adapter}
+  database: dizzynew_dizzydevelopment
+  username: #{database_username}
+  password: #{database_password}
+  host: #{database_hostname}
+
+# Warning: The database defined as 'test' will be erased and
+# re-generated from your development database when you run 'rake'.
+# Do not set this db to the same as development or production.
+test:
+  adapter: #{database_adapter}
+  database: dizzynew_dizzytest
+  username: #{database_username}
+  password: #{database_password}
+  host: #{database_hostname}
+
+production:
+  adapter: #{database_adapter}
+  database: dizzynew_dizzyproduction
+  username: #{database_username}
+  password: #{database_password}
+  host: #{database_hostname}
+EOF
 	
 	 # put database configuration in shared config dir
 	 run "mkdir -p #{shared_path}/config" 
 	 put database_configuration, "#{shared_path}/config/database.yml"
-	end
+	
 end
