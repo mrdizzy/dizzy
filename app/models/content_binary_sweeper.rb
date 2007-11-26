388106394917b3cc13beb260deda456c587e4a99
david.pettifer <david.pettifer@2420a164-f031-0410-b5c9-952b3439b043>
2007-11-26 11:46:16 +0000

    updated sweepers
    git-svn-id: http://dizzy.googlecode.com/svn/trunk@127 2420a164-f031-0410-b5c9-952b3439b043

--- app/models/content_binary_sweeper.rb
+++ app/models/content_binary_sweeper.rb
@@ -3,8 +3,17 @@ class ContentBinarySweeper < ActionController::Caching::Sweeper
 	observe ContentBinary
 	
 	def after_save(content_binary)
-		puts "yes!!!"
-		expire_page(:controller => "binaries", :action=> "get_cheatsheet_pdf", :permalink => content_binary.content.permalink, :extension => "pdf")
+expire_binary(content_binary.content.permalink)
+	
 	end
-
+	
+	def after_destroy(content_binary)
+		expire_binary(content_binary.content.permalink)
+	end
+	
+	private 
+	
+	def expire_binary(permalink)
+			expire_page(:controller => "binaries", :action=> "get_cheatsheet_pdf", :permalink => permalink, :extension => "pdf")
+	end
 end
