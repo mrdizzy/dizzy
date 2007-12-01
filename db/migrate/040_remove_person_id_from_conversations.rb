1d62eb6c80df959379dd3b8c73dd71e07261aa8c
david.pettifer <david.pettifer@2420a164-f031-0410-b5c9-952b3439b043>
2007-12-01 17:28:44 +0000

    removing unwanted migrations
    git-svn-id: http://dizzy.googlecode.com/svn/trunk@133 2420a164-f031-0410-b5c9-952b3439b043

--- db/migrate/040_remove_person_id_from_conversations.rb
+++
@@ -1,9 +0,0 @@
-class RemovePersonIdFromConversations < ActiveRecord::Migration
-  def self.up
-  	remove_column :conversations, :person_id
-  end
-
-  def self.down
-  	add_column :conversations, :person_id, :integer
-  end
-end
