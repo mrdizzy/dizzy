d6a44a572932c80376219a76217e86de02a36387
david.pettifer <david.pettifer@2420a164-f031-0410-b5c9-952b3439b043>
2007-12-01 19:16:44 +0000

    Clearing up migrations
    git-svn-id: http://dizzy.googlecode.com/svn/trunk@137 2420a164-f031-0410-b5c9-952b3439b043

--- db/migrate/007_add_article_id_to_comments.rb
+++
@@ -1,9 +0,0 @@
-class AddArticleIdToComments < ActiveRecord::Migration
-  def self.up
-  	add_column :comments, :article_id, :integer
-  end
-
-  def self.down
-  	drop_column :comments, :article_id
-  end
-end
--- db/migrate/008_add_date_to_comments.rb
+++
@@ -1,9 +0,0 @@
-class AddDateToComments < ActiveRecord::Migration
-  def self.up
-  	add_column :comments, :created_at, :datetime
-  end
-
-  def self.down
-  	drop_column :comments, :created_at
-  end
-end
