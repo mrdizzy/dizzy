1eca108876a9a2fd2371747097573fa451399ebc
david.pettifer <david.pettifer@2420a164-f031-0410-b5c9-952b3439b043>
2007-12-01 19:03:35 +0000

    Changed method of oading fictures/boostrapping
    git-svn-id: http://dizzy.googlecode.com/svn/trunk@135 2420a164-f031-0410-b5c9-952b3439b043

--- db/schema.rb
+++ db/schema.rb
@@ -2,7 +2,7 @@
 # migrations feature of ActiveRecord to incrementally modify your database, and
 # then regenerate this schema definition.
 
-ActiveRecord::Schema.define(:version => 10) do
+ActiveRecord::Schema.define(:version => 1) do
 
   create_table "addresses", :force => true do |t|
   end
