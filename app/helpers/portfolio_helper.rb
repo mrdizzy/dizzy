04d0d45ca9a8ec0237a1fff52499d8ed470d6eca
david.pettifer <david.pettifer@2420a164-f031-0410-b5c9-952b3439b043>
2007-12-31 14:34:28 +0000

    Refactored comments
    git-svn-id: http://dizzy.googlecode.com/svn/trunk@168 2420a164-f031-0410-b5c9-952b3439b043

--- app/helpers/portfolio_helper.rb
+++ app/helpers/portfolio_helper.rb
@@ -5,7 +5,7 @@ module PortfolioHelper
 		portfolio_items = PortfolioItem.find_all_by_company_id(@company.id, :conditions => 
     	"portfolio_types.visible = '1'", :include => "portfolio_type" )
 
-		portfolio_table = portfolio_items.collect { |p| ["<img src=\"/binaries/portfolio_image/#{p.id}.png\">", p.portfolio_type.column_space, "<img src=\"/binaries/portfolio_type/#{p.portfolio_type.id}\" alt=\"#{p.portfolio_type.description}\" />"]}
+		portfolio_table = portfolio_items.collect { |p| ["<img src=\"/binaries/portfolio_image/#{p.id}.png\">", p.portfolio_type.column_space, "<img src=\"/images/f/bullets/pixels/diamond.png\"> <img src=\"/binaries/portfolio_type/#{p.portfolio_type.id}\" alt=\"#{p.portfolio_type.description}\" />"]}
 
 		table(portfolio_table, {:columns => 3}, :class=> "portfolio_table")
 	end
