59461938983c4524479e4fa4f1b6cea4aa568074
david.pettifer <david.pettifer@2420a164-f031-0410-b5c9-952b3439b043>
2008-02-07 23:38:42 +0000

    Refactored comments
    git-svn-id: http://dizzy.googlecode.com/svn/trunk@200 2420a164-f031-0410-b5c9-952b3439b043

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -2,6 +2,7 @@ class CommentsController < ApplicationController
 	
 	def new
 		@comment = Comment.new
+		@comment.content_id = params[:content_id]
 		
 		respond_to do |wants|
 		 	wants.html
@@ -13,10 +14,16 @@ class CommentsController < ApplicationController
 	 	end
 	end
 	
-	def create
-		@content 	= Content.find(params[:content_id])			
-		@content.comments.create(params[:comment])
-		#comment.children.create(params[:comment])
+	def create		
+		if params[:comment_id]	
+			comment = Comment.find(params[:comment_id])
+			
+			comment.children.create(params[:comment])
+			
+		else
+			@content 	= Content.find(params[:content_id])		
+			@content.comments.create(params[:comment])
+		end
 	end
 
 end
