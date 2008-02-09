ac54bd091a42ecf08e6685cfb44a4e85a740d997
david.pettifer <david.pettifer@2420a164-f031-0410-b5c9-952b3439b043>
2008-02-09 11:26:26 +0000

    Navigation bar links
    git-svn-id: http://dizzy.googlecode.com/svn/trunk@213 2420a164-f031-0410-b5c9-952b3439b043

--- app/helpers/contents_helper.rb
+++ app/helpers/contents_helper.rb
@@ -242,5 +242,9 @@ module ContentsHelper
 		parser.parse
 		xml = listener.results		
 	end
+	
+	def pdf_thumbnail(content)
+		link_to image_tag(formatted_content_path(:category_id => content.main_category_permalink, :id => content.permalink, :format => "png")), formatted_content_path(:category_id => content.main_category_permalink, :id => content.permalink, :format => "pdf")
+	end
 
 end
