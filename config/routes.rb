39874a11aab1d0a51b5113b31b2f33b698026997
david.pettifer <david.pettifer@2420a164-f031-0410-b5c9-952b3439b043>
2008-02-10 20:43:31 +0000

    Updated routes for redirection
    git-svn-id: http://dizzy.googlecode.com/svn/trunk@226 2420a164-f031-0410-b5c9-952b3439b043

--- config/routes.rb
+++ config/routes.rb
@@ -11,7 +11,13 @@ ActionController::Routing::Routes.draw do |map|
 
   # You can have the root of your site routed by hooking up '' 
   # -- just remember to delete public/index.html.
+    
+    #Redirects
+     map.connect '/articles/rails-migrations.pdf', :controller => 'redirect', :url => '/ruby_on_rails/categories/migrations/contents/rails-migrations.pdf'       
     map.connect '/articles/rails-migrations', :controller => 'redirect', :url => '/ruby_on_rails/categories/migrations/contents/rails-migrations'
+    map.connect '/articles/beginning-file-uploads', :controller => 'redirect', :url => '/ruby_on_rails/categories/file-handling/contents/beginning-file-uploads'
+
+    
     map.latest 'ruby_on_rails/latest', :controller => "contents", :action => "index"    
     map.login 'login', :controller => "administrator_sessions", :action => "new"
 	map.connect '/', :controller => "welcome"
