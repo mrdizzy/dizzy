333140b42778ce7448be4cbd6221e1b6667c3625
david.pettifer <david.pettifer@2420a164-f031-0410-b5c9-952b3439b043>
2008-07-02 10:42:01 +0000

    Updated tests
    git-svn-id: http://dizzy.googlecode.com/svn/trunk@339 2420a164-f031-0410-b5c9-952b3439b043

--- test/unit/portfolio_item_test.rb
+++ test/unit/portfolio_item_test.rb
@@ -2,55 +2,70 @@ require File.dirname(__FILE__) + '/../test_helper'
 
 class PortfolioItemTest < Test::Unit::TestCase
   fixtures :portfolio_items
+  fixtures :companies
 
   # Replace this with your real tests.
   def test_truth
     assert true
   end
-
-  def test_valid_portfolio_item 
-  	valid_logo = PortfolioItem.new(	:portfolio_type_id 	=> "2",
-									:company_id 		=> "1",
-									:content_type 		=> "image/png",
-									:filename 			=> "letterhead.png",
-									:size 				=> "3644",
-									:data				=> portfolio_items(:dizzy_logo).data)
-													
-	assert valid_logo.save,  valid_logo.errors.full_messages
+  
+  def test_portfolio_item_must_be_less_than_200k
+  	flunk
   end
   
-  def test_unique_portfolio_type 
-  	invalid_logo = PortfolioItem.new( 	:portfolio_type_id 	=> "1",
-										:company_id 		=> "1",
-										:content_type 		=> "image/png",
-										:filename 			=> "colourlogo.png",
-										:data				=> portfolio_items(:dizzy_logo).data)
+  def test_portfolio_item_must_belong_to_valid_company
+  	#portfolio_item = portfolio_items(:heavenly_logo)
+  	#portfolio_item.company_id = 1005
+  	
+  	#assert !portfolio_item.save, portfolio_item.errors.full_messages
+  	flunk
+  end
+  
+  def test_portfolio_item_must_be_destroyed_upon_company_deletion
+  	portfolio_items = PortfolioItem.find_all_by_company_id(1)  
+  	assert_equal 7, portfolio_items.size
+  	company = Company.find(1)
+  	company.destroy
+	portfolio_items = PortfolioItem.find_all_by_company_id(1)  	
+	assert_equal 0, portfolio_items.size
+  end
+  
+  def test_portfolio_type_id_must_be_unique_to_company
+  	existing_portfolio_type = PortfolioItem.new( 	:portfolio_type_id 	=> "3",
+													:company_id 		=> "1",
+													:content_type 		=> "image/png",
+													:filename 			=> "colourlogo.png",
+													:data				=> portfolio_items(:heavenly_logo).data)
 													
-	assert !invalid_logo.save,  invalid_logo.errors.full_messages
+	assert !existing_portfolio_type.save,  existing_portfolio_type.errors.full_messages
   end    
   
-  def test_content_type 
+  def test_bad_portfolio_type_id_must_exist
+  	flunk
+  end
+  
+  def test_content_type_must_be_valid
   	bad 	= %w(image/jpg image/jpeg audio/wav image/tiff image/pngg image/giff image/png/audio)
   	good	= %w(image/png image/x-png image/gif)
   	
   	bad.each do |content_type| 
-		valid_content_type = PortfolioItem.new(	:portfolio_type_id	=> "3",
+		valid_content_type = PortfolioItem.new(	:portfolio_type_id	=> "2",
 												:company_id 		=> "1",
 												:content_type 		=> content_type,
 												:filename 			=> "letterhead.png",
 												:size 				=> "3644",
-												:data				=> portfolio_items(:dizzy_logo).data)
+												:data				=> portfolio_items(:heavenly_logo).data)
 														
 		assert !valid_content_type.valid?,  valid_content_type.errors.full_messages
 	end
 	
 	good.each do |content_type|
-		valid_content_type = PortfolioItem.new( :portfolio_type_id	=> "3",
+		valid_content_type = PortfolioItem.new( :portfolio_type_id	=> "2",
 												:company_id 		=> "1",
 												:content_type 		=> content_type,
 												:filename 			=> "letterhead.png",
 												:size 				=> "3644",
-												:data				=> portfolio_items(:dizzy_logo).data)
+												:data				=> portfolio_items(:heavenly_logo).data)
   		assert valid_content_type.valid?, valid_content_type.errors.full_messages
   	end
   end    
