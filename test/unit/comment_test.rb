88ea03ea26ec75930c6c4029a7e3ef23e04ab60a
david.pettifer <david.pettifer@2420a164-f031-0410-b5c9-952b3439b043>
2008-07-03 14:57:00 +0000

    Refactored comments test and added main comment mailer
    git-svn-id: http://dizzy.googlecode.com/svn/trunk@354 2420a164-f031-0410-b5c9-952b3439b043

--- test/unit/comment_test.rb
+++ test/unit/comment_test.rb
@@ -8,13 +8,31 @@ class CommentTest < Test::Unit::TestCase
     assert true
   end
   
-  def test_comment
-  	comment = comments(:bad_comment)
+  def test_should_fail_blank_comment
+  	comment = Comment.new
+  	assert !comment.valid?, comment.errors.full_messages
   end
   
-  def test_blank_comment
-  	comment = Comment.new
-  	assert !comment.valid?
+  def test_should_fail_invalid_email
+  	comment = comments(:parent_comment)
+  	
+  	bad_addresses = ["melinda@@dizzy.co.uk", "jamie@192.34.32.43", "louise@louise", "mr_habadashery@*bloo.com"]
+  	bad_addresses.each do |address|
+  		comment.email = address
+  		assert !comment.valid?, comment.errors.full_messages
+  		assert_equal "must contain a valid address", comment.errors.on(:email)
+  	end
+  end
+
+  def test_should_pass_valid_email
+  	comment = comments(:parent_comment)
+  	
+  	good_addresses = %w{ melinda@dizzy.co.uk jamie@dizzy.com louise.smith@germany.de mr_lewis@lewis.co.uk  david.pettifer@dizzy.co.uk jamie.han@motif-switcher.com }
+  	good_addresses.each do |address|
+  		comment.email = address
+  		assert comment.valid?, comment.errors.full_messages
+
+  	end
   end
   
 end
