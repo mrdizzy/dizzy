cf2ef56cd34c42b1c5560b5d7ebe92e50133deff
david.pettifer <david.pettifer@2420a164-f031-0410-b5c9-952b3439b043>
2008-08-01 16:03:38 +0000

    Refactored cheatsheets_controller_test.rb
    git-svn-id: http://dizzy.googlecode.com/svn/trunk@401 2420a164-f031-0410-b5c9-952b3439b043

--- test/functional/cheatsheets_controller_test.rb
+++ test/functional/cheatsheets_controller_test.rb
@@ -24,8 +24,6 @@ class CheatsheetsControllerTest < Test::Unit::TestCase
   def test_truth
     assert true
   end
-  
- 	#  Parameters: {"thumbnail"=>{"uploaded_data"=>""}, "commit"=>"", "cheatsheet"=>{"permalink"=>"action-mailer", "date(1i)"=>"2008", "date(2i)"=>"8", "title"=>"ActionMailer", "date(3i)"=>"1", "date(4i)"=>"14", "date(5i)"=>"24", "category_ids"=>["2"], "version_id"=>"1", "description"=>"Action Mailer cheatsheet", "new_version"=>""}, "action"=>"create", "controller"=>"cheatsheets", "pdf"=>{"uploaded_data"=>#<ActionController::UploadedStringIO:0x45ebc40>}}
  	
  	def test_should_create_on_valid_attributes
  		post :create, { :thumbnail 	=> 	{ :uploaded_data => fixture_file_upload("letterhead.png", "image/png") },
@@ -40,10 +38,9 @@ class CheatsheetsControllerTest < Test::Unit::TestCase
  										  :version_id => "1",
  										  :description => "Action Mailer cheatsheet",
  										  :new_version => "" },
- 						:pdf => { :uploaded_data => fixture_file_upload("letterhead.png", "image/png") } 
+ 						:pdf => { :uploaded_data => fixture_file_upload("letterhead.png", "image/pdf") } 
  					}, { :administrator_id => users(:mr_dizzy).id }
- 		assert assigns(:cheatsheet), "Should have assigned cheatsheet"
- 		assert assigns(:cheatsheet).user_id, "Should have assigned user"
+ 					
  		assert_response :redirect
 		assert_redirected_to cheatsheets_path
  	end
@@ -61,7 +58,7 @@ class CheatsheetsControllerTest < Test::Unit::TestCase
  										  :version_id => "1",
  										  :description => "Action Mailer cheatsheet",
  										  :new_version => "" },
- 						:pdf => { :uploaded_data => fixture_file_upload("letterhead.png", "image/png") } 
+ 						:pdf => { :uploaded_data => fixture_file_upload("letterhead.png", "image/pdf") } 
  					}, { :administrator_id => users(:mr_dizzy).id }
  					
  		assert assigns(:cheatsheet).errors.on(:thumbnail)
@@ -114,6 +111,32 @@ class CheatsheetsControllerTest < Test::Unit::TestCase
  		assert_equal 11, assigns(:cheatsheet).id
  		assert_response :success
  		assert_template "edit"
+ 	end
+ 	
+ 	def test_should_succeed_on_update_with_valid_attributes
+ 		post :update, { :id => "11", 
+ 						:thumbnail 	=> 	{ :uploaded_data => fixture_file_upload("letterhead.png", "image/png") },
+ 						:cheatsheet =>	{ :permalink => "action-two-mailer",
+ 										  "date(li)" => "2008",
+ 										  "date(2i)" => "8",
+ 										  "date(3i)" => "1",
+ 										  "date(4i)" => "14",
+ 										  "date(5i)" => "24",
+ 										  :title => "ActionMailer",
+ 										  :category_ids => ["2"],
+ 										  :version_id => "1",
+ 										  :description => "Action Mailer cheatsheet",
+ 										  :new_version => "" },
+ 						:pdf => { :uploaded_data => fixture_file_upload("letterhead.png", "application/pdf") } 
+ 					}, { :administrator_id => users(:mr_dizzy).id }
+ 		
+ 		assert_equal 0, assigns(:cheatsheet).errors.size, assigns(:cheatsheet).errors.full_messages
+ 		
+ 		assert_equal 0, assigns(:cheatsheet).pdf.errors.size, assigns(:cheatsheet).pdf.errors.full_messages 
+ 		
+ 		assert_equal 0, assigns(:cheatsheet).thumbnail.errors.size, assigns(:cheatsheet).thumbnail.errors.full_messages
+ 		assert_response :redirect
+		assert_redirected_to cheatsheet_path("action-two-mailer")
  	end
  	
 end
