2902bb6154637725482519cd47ad2b1a7fee47e0
david.pettifer <david.pettifer@2420a164-f031-0410-b5c9-952b3439b043>
2008-09-07 12:55:50 +0000

    Added note
    git-svn-id: http://dizzy.googlecode.com/svn/trunk@447 2420a164-f031-0410-b5c9-952b3439b043

--- app/controllers/application.rb
+++ app/controllers/application.rb
@@ -1,6 +1,9 @@
 # Filters added to this controller will be run for all controllers in the application.
 # Likewise, all the methods added will be available for all controllers.
 class ApplicationController < ActionController::Base
+	
+	# TODO Exception Notifier not working after upgrade to 2.1 
+	
 	include ExceptionNotifiable
 	
 	require_dependency 'content'
