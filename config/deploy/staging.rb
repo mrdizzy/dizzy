c2090a2bde828bfd260d99e741b4369d38fc1eea
david.pettifer <david.pettifer@2420a164-f031-0410-b5c9-952b3439b043>
2008-11-14 17:51:19 +0000

    git-svn-id: http://dizzy.googlecode.com/svn/trunk@505 2420a164-f031-0410-b5c9-952b3439b043

--- config/deploy/staging.rb
+++ config/deploy/staging.rb
@@ -15,7 +15,14 @@ namespace :deploy do
 	
 	desc "start the app server"
 	task :start, :roles => :app do
-	send(run_method, "cd #{current_path} && mongrel_rails start -d -p 12182 -e #{rails_env}")
+		send(run_method, "cd #{current_path} && mongrel_rails start -d -p 12182 -e #{rails_env}")
 	end
-
+	
+	
+	desc "import production database into staging"
+	task :copy_production_database, :roles => :app do 
+		send(run_method, "mysqldump -u #{database_username} --password=#{database_password} dizzynew_dizzyproduction > export.sql")
+		send(run_method, "mysql -u #{database_username} --password=#{database_password} dizzynew_dizzystaging < export.sql")
+	end
 end
+
