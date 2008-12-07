84bba2339c1928742a4b551dc66102fb8a8ab0bc
david.pettifer <david.pettifer@2420a164-f031-0410-b5c9-952b3439b043>
2008-12-07 13:04:47 +0000

    git-svn-id: http://dizzy.googlecode.com/svn/trunk@546 2420a164-f031-0410-b5c9-952b3439b043

--- app/controllers/portfolios_controller.rb
+++ app/controllers/portfolios_controller.rb
@@ -2,20 +2,11 @@ class PortfoliosController < ApplicationController
 	
 	def index
 		@company_pages, @companies = paginate :companies, :per_page => 8, :order => :name
-		respond_to do |wants|
-			wants.html
-			wants.js
-		end
 	end
 	
 	def show			
 		@company_pages, @companies = paginate :companies, :per_page => 8, :order => :name		
 		@company 	= Company.find(params[:id])		
     	@header 	= @company.portfolio_items.header
-    	
-    	respond_to do |wants|
-    		wants.html
-    		wants.js
-    	end
 	end  
 end
