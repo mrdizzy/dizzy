052527b3bdf63499327f35de1670d7adedea0ba9
david.pettifer <david.pettifer@2420a164-f031-0410-b5c9-952b3439b043>
2008-12-08 18:42:35 +0000

    git-svn-id: http://dizzy.googlecode.com/svn/trunk@562 2420a164-f031-0410-b5c9-952b3439b043

---
+++ app/controllers/administrator_sessions_controller.rb
@@ -0,0 +1,20 @@
+class AdministratorSessionsController < ApplicationController
+	
+	def create
+		session[:admin_password] = params[:admin_password]
+		if administrator?
+			flash[:notice] = "Successfully logged in"
+			redirect_to latest_path
+		else
+			flash[:notice] = "Invalid password"
+			render :action => "new"
+		end
+	end
+	
+	def destroy
+		reset_session
+		flash[:notice] = "Successfully logged out"
+		redirect_to latest_path
+	end
+
+end
