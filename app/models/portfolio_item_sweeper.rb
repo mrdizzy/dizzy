034435f99a409007c4ba70c37e5835f28dd48bde
david.pettifer <david.pettifer@2420a164-f031-0410-b5c9-952b3439b043>
2008-12-09 22:38:54 +0000

    git-svn-id: http://dizzy.googlecode.com/svn/trunk@579 2420a164-f031-0410-b5c9-952b3439b043

--- app/models/portfolio_item_sweeper.rb
+++ app/models/portfolio_item_sweeper.rb
@@ -3,11 +3,11 @@ class PortfolioItemSweeper < ActionController::Caching::Sweeper
   observe PortfolioItem
 
   def after_save(record)
-    expire_page hash_for_formatted_portfolio_items_path(:id => record.id, :format => :png)
+    expire_page hash_for_formatted_portfolio_item_path(:id => record.id, :format => :png)
   end
 
   def after_destroy(record)
-    expire_page hash_for_formatted_portfolio_items_path(:id => record.id, :format => :png)
+    expire_page hash_for_formatted_portfolio_item_path(:id => record.id, :format => :png)
   end
 
 end
--- test/integration/caching_test.rb
+++ test/integration/caching_test.rb
@@ -1,23 +1,8 @@
 require '../test_helper'
 
 class CachingTest < ActionController::IntegrationTest
-   fixtures :contents, :binaries, :categories, :versions, :portfolio_items, :companies, :portfolio_types
-
-  # Replace this with your real tests.
-  def test_truth
-    assert true
-  end
-  
-  def test_delete_cheatsheet
-  	  # post "/administrator_sessions", :admin_password => PASSWORD
-  	 #  assert_redirected_to latest_path
-  	   
-  	  # get "/ruby_on_rails/cheatsheets/action-mailer"
-  	 #  assert_response :success
-  	   
-
-  end
- 
+   fixtures :contents, :binaries, :categories, :versions,  :companies, :portfolio_types, :portfolio_items
+   
   def test_caching_of_cheatsheet_pages
   	  post "/administrator_sessions", :admin_password => PASSWORD
   	  assert_cache_pages("/ruby_on_rails/cheatsheets/action-mailer", 
@@ -34,5 +19,16 @@ class CachingTest < ActionController::IntegrationTest
  def test_caching_of_portfolio_items
   	  post "/administrator_sessions", :admin_password => PASSWORD
   	  assert_cache_pages("/portfolio_items/#{portfolio_items(:heavenly_slip).id}.png")
-  end  
+  	  
+  	  assert_expire_pages("/portfolio_items/#{portfolio_items(:heavenly_slip).id}.png") do |*urls|
+  	  	post "/portfolio_items/destroy/#{portfolio_items(:heavenly_slip).id}"
+  	  end
+  	  
+  	   assert_expire_pages("/portfolio_items/#{portfolio_items(:heavenly_slip).id}.png") do |*urls|
+  	   	puts portfolio_items(:heavenly_slip).company.id
+  	   	puts companies(:heavenly).id
+  	  	post "/companies/destroy/#{companies(:heavenly).id}"
+  	  end
+  end
+    
 end
