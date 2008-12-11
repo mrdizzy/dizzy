8aa5a85aaf7f3f01c81edc11f5a9d1ce9f4eb609
david.pettifer <david.pettifer@2420a164-f031-0410-b5c9-952b3439b043>
2008-12-11 11:16:06 +0000

    git-svn-id: http://dizzy.googlecode.com/svn/trunk@586 2420a164-f031-0410-b5c9-952b3439b043

--- app/controllers/portfolios_controller.rb
+++ app/controllers/portfolios_controller.rb
@@ -4,6 +4,11 @@ class PortfoliosController < ApplicationController
 	
 	def index
 		@companies = Company.paginate :per_page => 4, :order => :name, :page => params[:page]
+		@company = Company.first
+		respond_to do |wants|
+			wants.html { render :template => "portfolios/show"}
+			wants.js
+		end
 	end
 	
 	def show			
