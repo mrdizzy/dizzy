72be008f96e8d281c374190b3e70a1c7e9368da0
david.pettifer <david.pettifer@2420a164-f031-0410-b5c9-952b3439b043>
2008-12-11 10:38:14 +0000

    git-svn-id: http://dizzy.googlecode.com/svn/trunk@585 2420a164-f031-0410-b5c9-952b3439b043

--- app/controllers/portfolios_controller.rb
+++ app/controllers/portfolios_controller.rb
@@ -3,11 +3,11 @@ class PortfoliosController < ApplicationController
 	caches_page :show, :index
 	
 	def index
-		@companies = Company.paginate :per_page => 8, :order => :name, :page => params[:page], :renderer => 'RemoteLinkRenderer' 
+		@companies = Company.paginate :per_page => 4, :order => :name, :page => params[:page]
 	end
 	
 	def show			
-		@companies = Company.paginate :per_page => 8, :order => :name, :page => params[:page], :renderer => 'RemoteLinkRenderer' 
+		@companies = Company.paginate :per_page => 4, :order => :name, :page => params[:page]
 		@company 	= Company.find(params[:id])		
     	@header 	= @company.portfolio_items.header
 	end  
