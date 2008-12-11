b74a4fbb4a0006aebec5fc90d71efacb96952625
david.pettifer <david.pettifer@2420a164-f031-0410-b5c9-952b3439b043>
2008-12-11 16:36:56 +0000

    git-svn-id: http://dizzy.googlecode.com/svn/trunk@591 2420a164-f031-0410-b5c9-952b3439b043

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -3,27 +3,25 @@ class WelcomeController < ApplicationController
 	caches_page :index
 	
 	def index
-		paginate_logos
+		@logos = PortfolioItem.paginate :per_page => 1,  :page => params[:page], :conditions => [ "portfolio_type_id = ?", 4]
 		@articles 			= Content.recent.all(:limit => 4, :conditions => [ "contents.style != ? OR contents.style IS NULL", "SNIPPET" ] )
 		@main_article		= @articles.shift
 		@snippets			= Content.recent.snippets.all(:limit => 3)
-	end
-	
-	def next_logo
-		paginate_logos
-		render :update do |page|
-			page.visual_effect :fade, :latest_logo
-			page.delay(4) do
-				page.replace_html :latest_logo, :partial => "latest_logo"
-				page.visual_effect :appear, :latest_logo
-			end			
-			page.replace_html :logo_pagination, :partial => "logo_pagination"
+		respond_to do |wants|
+			wants.html
+			wants.js do
+				render :update do |page|
+					page.visual_effect :fade, :latest_logo
+					page.delay(4) do
+						page.replace_html :latest_logo, :partial => "latest_logo"
+						page.visual_effect :appear, :latest_logo
+					end			
+					page.replace_html :logo_pagination, :partial => "logo_pagination"
+				end
+			end
 		end
 	end	
 	
 	private
 	
-	def paginate_logos
-		@logo_pages, @logos = paginate :portfolio_items, :per_page => 1, :include => [:portfolio_type, :company], :conditions => ["portfolio_types.description = ? AND companies.visible = ?", "Logo", 1], :order => "portfolio_items.id desc"		
-	end
 end
