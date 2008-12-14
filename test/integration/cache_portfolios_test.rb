2dd053047b6810e82cfa5c10b460903aa8cb5ba6
david.pettifer <david.pettifer@2420a164-f031-0410-b5c9-952b3439b043>
2008-12-14 18:36:11 +0000

    git-svn-id: http://dizzy.googlecode.com/svn/trunk@596 2420a164-f031-0410-b5c9-952b3439b043

--- test/integration/cache_portfolios_test.rb
+++ test/integration/cache_portfolios_test.rb
@@ -4,25 +4,28 @@ class CachePortfoliosTest < ActionController::IntegrationTest
    fixtures :companies, :portfolio_items, :portfolio_types
    
   def test_should_cache_on_show
-  	assert_cache_pages("/portfolios/#{companies(:heavenly).id}")
   	assert_cache_pages("/portfolios/#{companies(:heavenly).id}.js")
   end 
   
   def test_should_cache_on_index
-  	assert_cache_pages("/portfolios.js")
+  	assert_cache_pages("/portfolios")
   end
   
+  def test_should_cache_pagination
+  	assert_cache_pages("portfolios/page/2.js")
+  end
+
   def test_should_expire_show_on_company_destroy
   	login
-  	assert_expire_pages("/portfolios/#{companies(:heavenly).id}", "/portfolios/#{companies(:heavenly).id}.js") do |*urls|
-  		post "/companies/destroy/#{companies(:heavenly).id}"
+  	assert_expire_pages("/portfolios/#{companies(:heavenly).id}.js") do |*urls|
+  		delete "/companies/#{companies(:heavenly).id}"
   	end
   end
-  
+
   def test_should_expire_show_on_portfolio_item_destroy
   	login
-  	assert_expire_pages("/portfolios/#{companies(:heavenly).id}", "/portfolios/#{companies(:heavenly).id}.js") do |*urls|
-  		post "/portfolio_items/destroy/#{portfolio_items(:heavenly_slip).id}"
+  	assert_expire_pages("/portfolios/#{companies(:heavenly).id}.js") do |*urls|
+  		delete "/portfolio_items/#{portfolio_items(:heavenly_slip).id}"
   	end
   end  
   
