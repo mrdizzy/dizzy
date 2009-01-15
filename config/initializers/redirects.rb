e956298281224f5694e4822b25eb2f02259e4c66
david.pettifer <david.pettifer@2420a164-f031-0410-b5c9-952b3439b043>
2009-01-15 18:58:14 +0000

    git-svn-id: http://dizzy.googlecode.com/svn/trunk@650 2420a164-f031-0410-b5c9-952b3439b043

--- config/initializers/redirects.rb
+++ config/initializers/redirects.rb
@@ -7,4 +7,6 @@ REDIRECTS = [ 	["/cheatsheets/rails-migrations.pdf", "/ruby_on_rails/cheatsheets
 				["/ruby_on_rails/cheatsheets/active-record-errors", "/ruby_on_rails/cheatsheets/active-record-validation-errors"],
 				["/ruby_on_rails/cheatsheets/active-record-errors.pdf", "/ruby_on_rails/cheatsheets/active-record-validation-errors.pdf"],
 				["/contents.rss", "/ruby_on_rails/contents.rss"],
-				["/portfolio/show", "/portfolios/1"]  ]
+				["/portfolio/show", "/portfolios/1"] ,
+				["/ruby_on_rails/cheatsheets", "/ruby_on_rails/categories/cheatsheets"],
+				["/cheatsheets", "/ruby_on_rails/categories/cheatsheets"] ]
