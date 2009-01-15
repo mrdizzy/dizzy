c4544e1a22db32daad1a5b411615fa8d5947c0d3
david.pettifer <david.pettifer@2420a164-f031-0410-b5c9-952b3439b043>
2009-01-15 13:38:02 +0000

    git-svn-id: http://dizzy.googlecode.com/svn/trunk@639 2420a164-f031-0410-b5c9-952b3439b043

--- app/helpers/comments_helper.rb
+++ app/helpers/comments_helper.rb
@@ -5,11 +5,10 @@ def prepare_comments(comments,result=Array.new, init=true, counter=0)
 	comments.each do |comment|	
 		result << "\n\n"
 		next if comment.parent_id && init	
-			
-		if counter.even?
-			result << "<div class=\"odd\" id=\"comment_#{comment.id}\">"
+		if comment.parent_id?
+			result << "<div class=\"single_comment\" id=\"comment_#{comment.id}\">"
 		else
-			result << "<div class=\"normal\" id=\"comment_#{comment.id}\">"
+			result << "<div class=\"root_comment\" id=\"comment_#{comment.id}\">"
 		end
 		
 		result << "<div class=\"comments_left\"><p><span class=\"posted_ago\">#{time_ago_in_words(comment.created_at).upcase} AGO</span><br/> <span class=\"email\">#{comment.name}</span></p>" 
@@ -25,9 +24,9 @@ def prepare_comments(comments,result=Array.new, init=true, counter=0)
 		result << "<p>" + link_to_remote("Delete", :url => comment_path(:content_id => comment.content.id, :id => comment.id), :method => :delete) + "</p>" if administrator? 
 					
 		result << 
-		"<div class=\"reply_comment\" id=\"reply_#{comment.id}\">" + 
+		"<div class=\"reply_comment\" id=\"reply_#{comment.id}\"><p>" + 
 		link_to_remote("Reply to this comment", { :url => new_content_comment_child_comment_path(:content_id => comment.content.id, :comment_id => comment.id), :method => :get }, :href => "http://www.yahoo.com")  + 
-		"</div><div id=\"reply_form_#{comment.id}\"></div>"
+		"</div><div id=\"reply_form_#{comment.id}\"></p></div>"
 			
 		prepare_comments(comment.children,result,false,counter) unless comment.children.empty?
 					
