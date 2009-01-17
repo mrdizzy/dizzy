edd84aa5920c4d07e8a3d938a31fdf84956e4227
david.pettifer <david.pettifer@2420a164-f031-0410-b5c9-952b3439b043>
2009-01-17 21:28:59 +0000

    git-svn-id: http://dizzy.googlecode.com/svn/trunk@666 2420a164-f031-0410-b5c9-952b3439b043

--- app/controllers/markdowns_controller.rb
+++ app/controllers/markdowns_controller.rb
@@ -1,5 +1,12 @@
 class MarkdownsController < ApplicationController
 	
+	def show
+		@content = Content.find(params[:id])
+		@content = Maruku.new(@content.content).to_html
+		@content= "<div id=\"article\">#{@content}</div>"
+		render :text => @content
+	end
+	
 	def create
 		@content = params[:content]
 		@content = Maruku.new(@content).to_html
