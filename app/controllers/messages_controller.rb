eb165967a88f1373f0012e1f473e60da65c0ca01
david.pettifer <david.pettifer@2420a164-f031-0410-b5c9-952b3439b043>
2009-01-26 11:59:27 +0000

    git-svn-id: http://dizzy.googlecode.com/svn/trunk@685 2420a164-f031-0410-b5c9-952b3439b043

--- app/controllers/messages_controller.rb
+++ app/controllers/messages_controller.rb
@@ -1,13 +1,17 @@
 class MessagesController < ApplicationController
 	
 	def create
-	puts params.to_yaml
 	
 		@message = Message.new(params[:message])
-		if @message.save		
+		@message.recaptcha_challenge_field = params[:recaptcha_challenge_field]
+		@message.remote_ip = request.remote_ip
+			
+		if @message.save
 			Mercury.deliver_new_message(@message)
+			@message = Message.new
 			render :update do |page|
 				page['contact_form'].toggle
+				page['contact_form'].replace_html :partial => "shared/contact_form"
 			end
 		else
 			render :update do |page|
--- app/models/message.rb
+++ app/models/message.rb
@@ -1,5 +1,19 @@
 class Message < ActiveRecord::Base
-		validates_presence_of :email, :name, :message
+	attr_accessor :recaptcha_response_field, :recaptcha_challenge_field, :remote_ip
+	
+	validates_presence_of :email, :name, :message, :recaptcha_response_field
 	validates_confirmation_of :email
 
+	def validate
+		recaptcha = Net::HTTP.post_form URI.parse("http://api-verify.recaptcha.net/verify"), {
+			:privatekey => RECAPTCHA_PRIVATE_KEY,
+			:remoteip   => remote_ip,
+			:challenge  => recaptcha_challenge_field,
+			:response   => recaptcha_response_field
+		}
+		result =  recaptcha.body
+		success,error = result.split("\n")
+		errors.add(:recaptcha_response_field, 'was not entered correctly') unless success == "true"
+	end
+		
 end
