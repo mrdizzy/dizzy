cf05a89c2a0a50ca927b356c8069406f00de9299
mrdizzy <david.pettifer@dizzy.co.uk>
2009-06-03 17:50:06 +0100

    Added model/mailers directory to environment.rb

--- config/environment.rb
+++ config/environment.rb
@@ -4,17 +4,24 @@
 # (Use only when you can't set environment variables through your web/app server)
 # ENV['RAILS_ENV'] = 'production'
 RAILS_GEM_VERSION = '2.1.1'
+
+# Recaptcha keys
 RECAPTCHA_PUBLIC_KEY = '6LfP1AQAAAAAAAKhfOUrKLj87PfBp77czQ87drhk'
 RECAPTCHA_PRIVATE_KEY = '6LfP1AQAAAAAAJLC4g8ticB9p5RuyEdq1QH4syc6'
+
 # Bootstrap the Rails environment, frameworks, and default configuration
 require File.join(File.dirname(__FILE__), 'boot')
 
 Rails::Initializer.run do |config|
   # Skip frameworks you're not going to use
-   config.frameworks -= [ :action_web_service ]
+  config.frameworks -= [ :action_web_service ]
 
   config.time_zone = 'London'
+  
+  # Add load paths for sweepers and mailers
   config.load_paths << "#{RAILS_ROOT}/app/models/sweepers"
+  config.load_paths << "#{RAILS_ROOT}/app/models/mailers"
+  
   config.action_controller.session = { :session_key => "_dizzy_session", :secret => "beaslewig175world66607814752865" }
   
   config.gem "maruku" 
