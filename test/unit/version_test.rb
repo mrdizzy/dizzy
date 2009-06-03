76051f3cee727431693f4ac0208f0e088da07e6d
unknown <David@.(none)>
2009-06-03 23:18:24 +0100

    Refactored version_test

--- test/unit/version_test.rb
+++ test/unit/version_test.rb
@@ -1,24 +1,24 @@
 require File.dirname(__FILE__) + '/../test_helper'
 
 class VersionTest < Test::Unit::TestCase
-	
-	fixtures :versions
 
   def test_truth
     assert true
   end
   
-  def test_should_fail_when_not_unique
-  	version = Version.new(:version_number => "2.0")
-  	version.valid?
-  	assert_equal "has already been taken", version.errors.on(:version_number)
+  def test_1_should_fail_when_not_unique
+  	version = Factory(:version)
+	version_duplicate = Factory.build(:version)
+	version_duplicate.version_number = version.version_number
+  	version_duplicate.valid?
+  	assert_equal "has already been taken", version_duplicate.errors.on(:version_number)
   end
   
-  def test_should_fail_when_not_valid_version_number
+  def test_2_should_fail_when_not_valid_version_number
 	flunk
   end
   
-	def test_should_succeed_when_valid_version_number		
+	def test_3_should_succeed_when_valid_version_number		
 	  flunk
   end
     
