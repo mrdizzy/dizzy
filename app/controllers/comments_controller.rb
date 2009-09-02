e140cc0780bb0be7f63ad2cd9b83623edd2ae744
mrdizzy <david.pettifer@dizzy.co.uk>
2009-09-02 13:49:26 +0100

    Updated

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -31,15 +31,15 @@ class CommentsController < ApplicationController
 		@comment = Comment.new(params[:comment].merge({:content_id => params[:content_id]}))
      
 		if @comment.save
-         render :update do |page|
-            page.replace "new_comment", :partial => 'comment', :object => @comment 
+      render :update do |page|
+        page.replace "new_comment", :partial => 'comment', :object => @comment 
 			end
-      else
-         render :update do |page|
-            page.replace_html "new_comment", :partial => 'comment_form'
-         end
+    else
+      render :update do |page|
+        page.replace_html "new_comment", :partial => 'comment_form'
       end
-      
+    end  
+    
 	end
 
 end
