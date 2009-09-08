9610c64556eee5938cf1c9065a48d4f309335c26
mrdizzy <david.pettifer@dizzy.co.uk>
2009-09-08 18:40:41 +0100

    Textcaptcha

--- vendor/plugins/text_captcha/lib/text_captcha.rb
+++ vendor/plugins/text_captcha/lib/text_captcha.rb
@@ -14,9 +14,13 @@ module TextCaptcha
     end
     
     module ClassMethods
+    
+       ActiveRecord::Base.before_validation :remove_whitespace
+    
        ActiveRecord::Base.validates_each :answer do |record, attr, value|
-        record.errors.add attr, "is not correct" unless record.answer_confirmation.values.detect { |v| v[:answer_confirmation] == record.encrypted_answer }
+        record.errors.add attr, "is not the correct answer." unless record.answer_confirmation.values.detect { |v| v[:answer_confirmation] == record.encrypted_answer }
        end
+       
     end 
     
     module InstanceMethods
@@ -33,6 +37,10 @@ module TextCaptcha
       end
       
       private
+      
+      def remove_whitespace
+        self.answer = self.answer.strip
+      end
       
       def get_question
         result = Net::HTTP.get(URI.parse("http://textcaptcha.com/api/1l9vs903fvj4coc48cc0gckw8"))
--- vendor/plugins/text_captcha/test/text_captcha_test.rb
+++ vendor/plugins/text_captcha/test/text_captcha_test.rb
@@ -7,8 +7,6 @@ require 'text_captcha'
 require 'captcha_view'
 require 'init'
 
-#require 'action_view/test_case'
-
 ActiveRecord::Base.establish_connection(:adapter => "sqlite3", :database => ":memory:")
 
 ActiveRecord::Schema.define do 
@@ -34,7 +32,7 @@ class CaptchaTest < ActionView::TestCase
   def test_1_forms
   
     form_for Book.new do |f|
-      #puts f.captcha
+      puts f.captcha
     end
     
   end
@@ -46,26 +44,35 @@ class CaptchaTest < ActionView::TestCase
 end
 
 class TestSubmission < ActiveSupport::TestCase
-  answer = "James \n"
-  answer_confirmation = "b4cc344d25a2efe540adbf2678e2304c"
-  book = Book.new(:title => "Generation X", :answer => answer, :answer_confirmation =>  { 0 => { :answer_confirmation => answer_confirmation } } )
 
-  answer= "50414"
-  answer_confirmation = "d1599afc9321bf94d0b209551ad6e87f" 
-  book = Book.new(:title => "Generation X", :answer => answer, :answer_confirmation => { 0 => { :answer_confirmation => answer_confirmation } } )
-    book.save
-    #puts book.encrypted_answer
-    puts 
-   #puts book.answer_confirmation.values.detect { |v| v == book.encrypted_answer }
-   puts book.valid?
-   
-  possible_answers = ["twenty eight\r", "28\n"]
-  answer_1 = "33e75ff09dd601bbe69f351039152189"
-  answer_2 = "c3d206029972e8f24543aaab6a24f48c"
- 
-  possible_answers.each do |answer|
-    book = Book.new(:title => "Generation X", :answer => "twenty eight", :answer_confirmation => { 0 => { :answer_confirmation => answer_1 }, 1 => { :answer_confirmation => answer_2 } } )    
-
+  def test_correct_submissions
+   answer = " 99107\n\r"
+   answer_confirmation = "caa1ca78b3cce39fd93722159cba55cb"
+   book = Book.new(:title => "Generation X", :answer => answer, :answer_confirmation =>  { 0 => { :answer_confirmation => answer_confirmation } } )
+   assert book.valid?, "#{book.title} should be valid: #{book.errors.full_messages}"
+  
+    answer = " \n50414\n"
+    answer_confirmation = "d1599afc9321bf94d0b209551ad6e87f" 
+    book = Book.new(:title => "Girlfriend In A Coma", :answer => answer, :answer_confirmation => { 0 => { :answer_confirmation => answer_confirmation } } )
+    assert book.valid?, "#{book.title} should be valid: #{book.errors.full_messages}"
+  
+    possible_answers = ["  twenty eight\r", " 28\n\n "]
+    answer_1 = "33e75ff09dd601bbe69f351039152189"
+    answer_2 = "c3d206029972e8f24543aaab6a24f48c"
+  
+    possible_answers.each do |answer|
+      book = Book.new(:title => "All Families Are Psychotic", :answer => answer, :answer_confirmation => { 0 => { :answer_confirmation => answer_1 }, 1 => { :answer_confirmation => answer_2 } } )    
+       assert book.valid?, "#{book.title} should be valid: #{book.errors.full_messages}"
+    end
+  end
+  
+  def test_invalid_submissions
+   answer = "99102"
+   answer_confirmation = "caa1ca78b3cce39fd93722159cba55cb"
+   book = Book.new(:title => "Generation X", :answer => answer, :answer_confirmation =>  { 0 => { :answer_confirmation => answer_confirmation } } )
+   assert !book.valid?, "#{book.title} should be invalid"
+   assert book.errors.count, 1
+   assert_equal "is not the correct answer.", book.errors[:answer]
   end
 
 end
