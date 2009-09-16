41c72459cf35f3e904033d2bd82814e235196729
unknown <David@.(none)>
2009-09-16 20:50:01 +0100

    Refactored

--- vendor/plugins/acts_as_binary/test/acts_as_binary_test.rb
+++ vendor/plugins/acts_as_binary/test/acts_as_binary_test.rb
@@ -10,6 +10,7 @@ ActiveRecord::Schema.define do
     t.integer :image_size
     t.string :image_content_type
     t.string :image_filename
+    t.string :type
   end
   create_table :categories do |t| 
     t.string :name
@@ -22,42 +23,26 @@ ActionController::Routing::Routes.draw do |map|
   end
 end
 
-module Dizzy
-  module FileUploadToDb
-    def self.included(base)
-      base.extend(ClassMethods)
-      base.send(:include, InstanceMethods)
-    end
-     
-    module ClassMethods      
-    
-    end
-  
-    module InstanceMethods
-   
+ActiveRecord::Base.connection.tables.each do |table|
+  klass = table.singularize.camelize
+  eval("#{klass} = Class.new(ActiveRecord::Base)")
+  eval("#{klass}.columns").each do |col|
+    if col.type == :binary
+      eval("#{klass}").class_eval <<-EOV
+        def #{col.name}=(input)                                     # def image=(input)
+          unless input.blank?                                       #   unless input.blank?
+            #{col.name}_size = input.size                           #     image_size = input.size
+            #{col.name}_filename = input.original_filename          #     image_filename = input.original_filename
+            #{col.name}_content_type = input.content_type           #     image_content_type = input.content_type
+            write_attribute(:#{col.name}, input.read)               #     write_attribute(:image, input.read)
+          end                                                       #   end
+        end                                                         # end
+      EOV
     end
   end
 end
-
-ActiveRecord::Base.send(:include, Dizzy::FileUploadToDb)
-
+  
 class Product < ActiveRecord::Base
-
- self.columns.each do |col|
-  if col.type == :binary
-    src = <<-end_src
-    def #{col.name}=(uploaded_file_object)
-      unless uploaded_file_object.blank?
-      self.#{col.name}_size = uploaded_file_object.size
-      self.#{col.name}_filename = uploaded_file_object.original_filename
-      self.#{col.name}_content_type = uploaded_file_object.content_type
-      write_attribute(:#{col.name}, uploaded_file_object.read)
-      end
-    end
-end_src
-class_eval src
-  end
-  end
   validates_presence_of :image
   belongs_to :category 
 end
@@ -68,59 +53,59 @@ class Category < ActiveRecord::Base
 end
 
 class ActsAsBinaryTest < ActiveSupport::TestCase
-
+  
   def setup
-  @valid_product = Product.create!(:name => "Pepsi Cola", :image => ActionController::TestUploadedFile.new("letterhead.png", "image/png"))
-  @valid_category = Category.create!(:name => "Category")
+    @valid_product = Product.create!(:name => "Pepsi Cola", :image => ActionController::TestUploadedFile.new("letterhead.png", "image/png"))
+    @valid_category = Category.create!(:name => "Category")
   end
-
+  
   def teardown
-  Product.delete_all
-  Category.delete_all
+    Product.delete_all
+    Category.delete_all
   end
 
   def test_1_should_succeed_on_update_with_valid_attributes
-  @valid_product.update_attributes(:name => "Coca Cola", :image => ActionController::TestUploadedFile.new("compliment.png", "image/png"))
-  assert @valid_product.valid?
-  assert_equal ActionController::TestUploadedFile.new("compliment.png", "image/png").read, @valid_product.image
-  assert_equal "Coca Cola", @valid_product.name
+    @valid_product.update_attributes(:name => "Coca Cola", :image => ActionController::TestUploadedFile.new("compliment.png", "image/png"))
+    assert @valid_product.valid?
+    assert_equal ActionController::TestUploadedFile.new("compliment.png", "image/png").read, @valid_product.image
+    assert_equal "Coca Cola", @valid_product.name
   end
-
+  
   def test_3_should_succeed_on_create_with_nested_model 
-  @valid_category.products_attributes = [ { :name => "Fanta", :image => ActionController::TestUploadedFile.new("letterhead.png", "image/png") } ]
-
-  child_product = @valid_category.products.first
-
-  assert_equal @valid_category.products.size, 1
-  assert_equal "Fanta", child_product.name
+    @valid_category.products_attributes = [ { :name => "Fanta", :image => ActionController::TestUploadedFile.new("letterhead.png", "image/png") } ]
+   
+    child_product = @valid_category.products.first
+    
+    assert_equal @valid_category.products.size, 1
+    assert_equal "Fanta", child_product.name
   end
-
+  
   def test_4_should_succeed_on_update_with_nested_model
-  @valid_category.products << @valid_product
-  @valid_category.save!
-
-  @valid_category.products_attributes = [ { :name => "Lucozade", :id => @valid_product.id } ]
-  assert_equal "Lucozade", @valid_category.products.first.name
-  assert_equal ActionController::TestUploadedFile.new("letterhead.png", "image/png").read, @valid_category.products.first.image
+    @valid_category.products << @valid_product
+    @valid_category.save!
+    
+    @valid_category.products_attributes = [ { :name => "Lucozade", :id => @valid_product.id } ]
+    assert_equal "Lucozade", @valid_category.products.first.name
+    assert_equal ActionController::TestUploadedFile.new("letterhead.png", "image/png").read, @valid_category.products.first.image
   end
-
+  
   def test_5_should_succeed_on_update_with_nested_model_and_empty_file_upload
-  @valid_category.products << @valid_product
-  @valid_category.save!
-
-  @valid_category.products_attributes = [ { :name => "7UP", :image => "", :id => @valid_product.id } ]
-  @valid_category.save!
-
-  assert_equal "7UP", @valid_category.products.first.name
-  assert_equal ActionController::TestUploadedFile.new("letterhead.png", "image/png").read, @valid_category.products.first.image
+    @valid_category.products << @valid_product
+    @valid_category.save!
+   
+    @valid_category.products_attributes = [ { :name => "7UP", :image => "", :id => @valid_product.id } ]
+    @valid_category.save!
+    
+    assert_equal "7UP", @valid_category.products.first.name
+    assert_equal ActionController::TestUploadedFile.new("letterhead.png", "image/png").read, @valid_category.products.first.image
 
   end
-
+  
   def test_6_should_fail_on_create_with_empty_file_upload
-  product = Product.new(:name => "Pepsi Cola", :image => "")
-  assert !product.valid?
-  assert_equal 1, product.errors.size, product.errors.size
-  assert_equal "can't be blank", product.errors[:image], product.errors.full_messages
+    product = Product.new(:name => "Pepsi Cola", :image => "")
+    assert !product.valid?
+    assert_equal 1, product.errors.size, product.errors.size
+    assert_equal "can't be blank", product.errors[:image], product.errors.full_messages
   end
 
 end
@@ -128,19 +113,19 @@ end
 class FileFieldTest < ActionView::TestCase
 
   def test_file_field
-
-  @product = Product.new
-  @category = Category.create(:name => "Televisions")
-
+  
+    @product = Product.new
+    @category = Category.create(:name => "Televisions")
+   
   result = form_for [@category, @product] do |f|
-    p f.text_field :name
-    p f.file_field :image
+      p f.text_field :name
+      p f.file_field :image
+    end
+    p result
   end
-  p result
-  end
-
+  
   def protect_against_forgery?
-  false
+    false
   end
 
 end
