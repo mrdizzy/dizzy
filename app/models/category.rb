c6b612c90c1d1081ebe082cff8b381f3043167cb
unknown <David@.(none)>
2010-01-28 01:40:00 +0000

    Updated

--- app/models/category.rb
+++ app/models/category.rb
@@ -7,12 +7,3 @@ class Category < ActiveRecord::Base
 	validates_uniqueness_of 	:permalink, :name, :allow_blank => true
 	validates_format_of			:permalink, :with => /^[a-z0-9-]+$/, :allow_blank => true
 end
-
-# == Schema Info
-# Schema version: 20090918003951
-#
-# Table name: categories
-#
-#  id        :integer(4)      not null, primary key
-#  name      :string(255)
-#  permalink :string(255)
