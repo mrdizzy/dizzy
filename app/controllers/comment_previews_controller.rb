1e83682619b15a79ab99930af67a7c5fe15efc8a
unknown <David@.(none)>
2010-02-15 11:31:00 +0000

    Updated

---
+++ app/controllers/comment_previews_controller.rb
@@ -0,0 +1,20 @@
+class CommentPreviewsController < ApplicationController
+	
+	def create			
+		@comment = Comment.new(params[:comment])
+    
+    respond_to do |format|     
+      format.js do
+          if @comment.valid?
+            render :partial => "comment_preview"
+		  else
+            render :partial => "comment_preview_form"
+          end
+        
+      end
+      
+    end
+      
+	end
+
+end
