00292d860d5fff246086b3d264fb7d7dec6e88dc
unknown <David@.(none)>
2010-02-17 20:39:56 +0000

    Cleaning up CSS

--- app/helpers/portfolios_helper.rb
+++ app/helpers/portfolios_helper.rb
@@ -2,8 +2,8 @@ module PortfoliosHelper
 	
 	def portfolio_table(columns=3)	
 		portfolio_items = @company.portfolio_items_for_display
-		portfolio_table = portfolio_items.collect { |p| [ image_tag(formatted_portfolio_item_path(p.id, :png)), p.portfolio_type.column_space, "<h2>" + diamond + " " + p.portfolio_type.description + "</h2>"]}
-		table(portfolio_table, {:columns => columns}, :class=> "portfolio_table")
+		portfolio_table = portfolio_items.collect { |p| [ image_tag(formatted_portfolio_item_path(p.id, :png)), p.portfolio_type.column_space, diamond + " " + p.portfolio_type.description]}
+		table(portfolio_table, {:columns => columns}, :id => "portfolio_table")
 	end
 	
 end
