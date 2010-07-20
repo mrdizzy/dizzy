25c55b7d66c010a4cfb86da4e63f18ad737fe89e
mr_dizzy <david.pettifer@dizzy.co.uk>
2010-07-20 22:11:58 +0100

    Starting migration to rails 3

---
+++ app/helpers/application_helper_old.rb
@@ -0,0 +1,14 @@
+module ApplicationHelper
+
+    def article_link(object); link_to object.title, content_path(object.permalink); end
+
+	def posted_in(categories, and_connector = true)
+		and_connector = and_connector ? "<span class=\"amp\">&asp;</span>" : ", "
+		categories.map! { |c| link_to(c.name.upcase, category_path(c.permalink)) }
+		result = categories.to_sentence(:last_word_connector => and_connector, :two_words_connector => ", ")
+		"<span class=\"posted_in\">" + result + "</span>"
+	end
+
+	def administrator?() 		session[:administrator_id] ? true : false; end
+	
+end
---
+++ test/test_helper_old.rb
@@ -0,0 +1,25 @@
+ENV["RAILS_ENV"] = "test"
+require File.expand_path(File.dirname(__FILE__) + "/../config/environment")
+require 'test_help'
+require 'pp'
+
+class ActiveSupport::TestCase
+
+  self.use_transactional_fixtures = true
+  self.use_instantiated_fixtures  = false
+  	 
+  def savefile(input)
+    aFile = File.new("file.html", "w")
+    aFile.write(input)
+    aFile.close
+  end
+   
+  def admin_pass
+    { :admin_password => PASSWORD }
+  end
+
+  def login
+  	 post "/administrator_sessions", admin_pass
+  end
+  
+end
