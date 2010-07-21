f1a5bbe055817d9ce9922006639773db5116a373
unknown <David@.(none)>
2010-07-21 15:18:59 +0100

    Updated

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,6 +1,6 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery
-  layout 'application'
+
   
     helper :all 
   helper_method :administrator?
--- app/helpers/icons_helper.rb
+++ app/helpers/icons_helper.rb
@@ -1,11 +1,11 @@
 module IconsHelper
-	def location_icon() 	image_tag("f/small_icons/you-are-here.gif", :alt => "You are here:", :size=> "25x24"); end	
-	def asterisk() 			image_tag("f/bullets/pixels/asterisk.png", :alt => "*"); end
-	def square()			image_tag("f/bullets/pixels/square.png", :alt => "&mdash;"); end
-	def bigarrow() 			image_tag("f/bullets/pixels/bigarrow.png", :size=> "17x13", :alt => "->"); end	
-	def bigleftarrow() 		image_tag("f/bullets/arrows/bigleftarrow.png", :size => "17x13", :alt => "->"); end		
-	def diamond() 			image_tag("f/bullets/pixels/diamond.png", :size=> "5x5", :alt => "*"); end
-	def solid_arrow() 		image_tag("f/bullets/arrows/solidarrow.png", :size => "13x9", :alt => ">"); end
-	def spiro() 			"<div class=\"spiro\">&nbsp;</div>"; end	
+	def location_icon() 	image_tag("f/small_icons/you-are-here.gif", :alt => "You are here:", :size=> "25x24").html_safe; end	
+	def asterisk() 			image_tag("f/bullets/pixels/asterisk.png", :alt => "*").html_safe; end
+	def square()			image_tag("f/bullets/pixels/square.png", :alt => "&mdash;").html_safe; end
+	def bigarrow() 			image_tag("f/bullets/pixels/bigarrow.png", :size=> "17x13", :alt => "->").html_safe; end	
+	def bigleftarrow() 		image_tag("f/bullets/arrows/bigleftarrow.png", :size => "17x13", :alt => "->").html_safe; end		
+	def diamond() 			image_tag("f/bullets/pixels/diamond.png", :size=> "5x5", :alt => "*").html_safe; end
+	def solid_arrow() 		image_tag("f/bullets/arrows/solidarrow.png", :size => "13x9", :alt => ">").html_safe; end
+	def spiro() 			"<div class=\"spiro\">&nbsp;</div>".html_safe; end	
 	def visible?(input) 	diamond if input; end 	
 end
